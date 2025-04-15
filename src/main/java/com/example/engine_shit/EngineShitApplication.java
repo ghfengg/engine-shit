78244da3cde92ce5aada955bec008e38b922ae7c
yangfeng <ccnn1987@gmail.com>
2025-04-15 17:55:31 +0800

    init

---
+++ src/main/java/com/example/engine_shit/EngineShitApplication.java
@@ -0,0 +1,13 @@
+package com.example.engine_shit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineShitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineShitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/engine_shit/EngineShitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.engine_shit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineShitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
